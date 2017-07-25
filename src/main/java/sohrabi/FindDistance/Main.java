705de0b099d213f9aae10320de7f7a136d8b0394
shahabsohrabi <shb.sohrabi@gmail.com>
2017-07-25 23:07:20 +0300

    modified

--- src/main/java/sohrabi/FindDistance/Main.java
+++ src/main/java/sohrabi/FindDistance/Main.java
@@ -1,34 +1,32 @@
 /**
- * Calculate the distance between indexes 
- * of the smallest and the second smallest 
+ * Calculate the distance between indexes
+ * of the smallest and the second smallest
  * numbers in an array.
+ *
  * @author (S. Sohrabi)
  * @version (1.0)
  */
 
 public class Main {
-	public static void main(String[] args) 
-    {
-        int[] elements  = {23, 45, 34, 12, 45, 4, 38, 56, 2, 49, 100}; 
-        int smallest = elements[0]; 
-        int secondSmallest = elements[0]; 
-        int p1 = 0;
-        int p2 = 0;
-        
-        for (int i = 0; i < elements.length; i++)
-        {
+    public static void main(String[] args) {
+        int[] elements = {23, 45, 34, 12, 45, 4, 38, 56, 2, 49, 100};
+        int smallest = elements[0];
+        int secondSmallest = elements[0];
+        int smallestIndex = 0;
+        int secondSmallestIndex = 0;
 
-            if (elements[i] < smallest)
-            {
+        for (int i = 0; i < elements.length; i++) {
+
+            if (elements[i] < smallest) {
                 secondSmallest = smallest;
-                p2 = p1;
+                secondSmallestIndex = smallestIndex;
                 smallest = elements[i];
-                p1 = i;
+                smallestIndex = i;
 
             }
         }
-        int distance= p2-p1;
-        System.out.println(distance);    
+        int distance = secondSmallestIndex - smallestIndex;
+        System.out.println(distance);
     }
 
 }
